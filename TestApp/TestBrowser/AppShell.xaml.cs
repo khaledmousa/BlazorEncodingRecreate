71e194690e05f8fa0a3aa0b15c656be55af43040
khaledmousa <khaled.mousa@gmail.com>
2025-08-05 12:37:34 +0100

    using hybridwebview

---
+++ TestApp/TestBrowser/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace TestBrowser
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ TestApp/TestBrowser/MainPage.xaml.cs
@@ -0,0 +1,27 @@
+﻿namespace TestBrowser
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+            hybridWebView.SetInvokeJavaScriptTarget(this);
+        }
+
+
+        private void OnHybridWebViewRawMessageReceived(object sender, HybridWebViewRawMessageReceivedEventArgs e)
+        {
+            MainThread.BeginInvokeOnMainThread(async () => await DisplayAlert("Raw Message Received", e.Message, "OK"));
+        }
+
+
+        public void ShowMessage(string s)
+        {
+            MainThread.BeginInvokeOnMainThread(async () => await DisplayAlert("Show Message called", s, "OK"));
+
+        }
+
+    }
+}
